311b92a35ab0e13ace23d04c51f6c98f5e8526c2
vktryy <reneva_v@mail.ru>
2025-07-01 14:19:56 +0300

    solution

--- src/human.cpp
+++ src/human.cpp
@@ -1,3 +1,21 @@
-#include "human.h"
+#include "hybrid.h"
+#include <iostream>
 
-// your code here
+Human::Human() : name("Unknown"), age(0), alive_status(true) {}
+
+Human::Human(const std::string& name, int age) : name(name), age(age), alive_status(true) {}
+
+void Human::die() {
+    alive_status = false;
+    std::cout << name << " умер." << std::endl;
+}
+
+void Human::becomeSupernatural() {
+    std::cout << name << " превращается в сверхъестественное существо!" << std::endl;
+}
+
+std::string Human::getName() const { return name; }
+
+int Human::getAge() const { return age; }
+
+bool Human::isAlive() const { return alive_status; }
